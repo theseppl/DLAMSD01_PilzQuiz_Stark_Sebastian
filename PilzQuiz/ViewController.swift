5c18ce0d4616df87615c300a05ebb5a37bc6bb1e
Sebastian Stark <sstark@gmx.net>
2026-01-27 22:08:29 +0100

    Fehler an Verknüpfung ImageView behoben

--- PilzQuiz/ViewController.swift
+++ PilzQuiz/ViewController.swift
@@ -23,8 +23,8 @@ class ViewController: UIViewController {
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
         updateElement()
+        super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
