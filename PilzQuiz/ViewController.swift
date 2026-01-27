e4dae646aa0483c622a6453d52e0a1be6019d75c
Sebastian Stark <sstark@gmx.net>
2026-01-27 21:47:23 +0100

    Daten und Bilder eingefügt II

--- PilzQuiz/ViewController.swift
+++ PilzQuiz/ViewController.swift
@@ -8,9 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    func updateElement() {
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+        answerLabel.text = "?"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateElement()
         // Do any additional setup after loading the view.
     }
 
