5166f4293f63dcd051e7820e81f1f55746464d1b
Sebastian Stark <sstark@gmx.net>
2026-01-28 20:52:23 +0100

    TextField und SegmentControl implementiert

--- PilzQuiz/ViewController.swift
+++ PilzQuiz/ViewController.swift
@@ -7,19 +7,33 @@
 
 import UIKit
 
+enum Mode {
+    case flashCard
+    case quiz
+}
+
 class ViewController: UIViewController {
     
     let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
     var currentElementIndex = 0
+    var mode: Mode = .flashCard
     
     @IBOutlet var imageView: UIImageView!
     @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var modeSelector: UISegmentedControl!
+    @IBOutlet weak var textField: UITextField!
+    
     
     @IBAction func showAnswer(_ sender: Any) {
         answerLabel.text = elementList[currentElementIndex]
     }
     
     @IBAction func next(_ sender: Any) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        updateElement()
     }
     
     
