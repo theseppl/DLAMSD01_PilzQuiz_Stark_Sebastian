033350b4c3ef0305e7273226c41638450b1ead8d
Sebastian Stark <sstark@gmx.net>
2026-02-09 20:41:52 +0100

    BackGroundViewController detailliert kommentiert

--- PilzQuiz/BackgroundViewController.swift
+++ PilzQuiz/BackgroundViewController.swift
@@ -8,21 +8,33 @@
 //import Foundation
 import UIKit
 
+/*
+ Ein gemeinsamer ViewController für alle Screens der App.
+ Zweck: Jeder Screen, der von `BackgroundViewController` erbt,
+ erhält automatisch ein Hintergrundbild.
+ Dadurch muss der Code nicht in jedem ViewController dupliziert werden.
+ */
+
 class BackgroundViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         addBackgroundImage()
     }
 
+    // Fügt ein Hintergrundbild ein.
     func addBackgroundImage() {
+        
+        // Erstellt ein UIImageView, das den gesamten View‑Bereich abdeckt.
         let backgroundImage = UIImageView(frame: view.bounds)
         backgroundImage.image = UIImage(named: "Hintergrundbild")
         backgroundImage.contentMode = .scaleAspectFill
         backgroundImage.clipsToBounds = true
         backgroundImage.translatesAutoresizingMaskIntoConstraints = false
 
+        // Das Bild als unterstes Element einfügen
         view.insertSubview(backgroundImage, at: 0)
 
+        // Das Bild soll exakt an allen vier Seiten anliegen.
         NSLayoutConstraint.activate([
             backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
             backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
